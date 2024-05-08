4a0844841ee0da5675f7d98e4e7e7e59a29133b1
MD Shahadat Hossain <sharman.bd0@gmail.com>
2024-05-08 12:43:31 +0600

    Completed assignmentOne

--- assignmentOne/JsonFormattingAssignment/JsonFormatter.cs
+++ assignmentOne/JsonFormattingAssignment/JsonFormatter.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +10,105 @@ namespace JsonFormattingAssignment
 {
     public static class JsonFormatter
     {
+        public static string Serialize(object obj)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            SerializeValue(obj, stringBuilder);
+            return stringBuilder.ToString();
+        }
 
+        private static void SerializeValue(object obj, StringBuilder stringBuilder)
+        {
+            if (obj == null)
+            {
+                stringBuilder.Append("null");
+                return;
+            }
+            Type type = obj.GetType();
+
+            if (PrimitiveData(type))
+            {
+                SerializePrimitive(obj, stringBuilder);
+            }
+            else if (type.IsArray || type.GetInterfaces().Contains(typeof(IEnumerable)))
+            {
+                SerializeArrayOrList(obj, stringBuilder);
+            }
+            else if (type.IsClass)
+            {
+                SerializeObjet(obj, stringBuilder);
+            }
+        }
+
+        private static void SerializePrimitive(object obj, StringBuilder stringBuilder)
+        {
+            if (obj is string)
+            {
+                stringBuilder.Append($"\"{InputString((string)obj)}\"");    
+            }
+            else if (obj is DateTime)
+            {
+                stringBuilder.Append($"\"{((DateTime)obj).ToString("yyyy-MM-dd")} \"");
+            }
+            else
+            {
+                stringBuilder.Append(obj);
+            }
+        }
+
+        private static void SerializeArrayOrList(object obj, StringBuilder stringBuilder)
+        {
+            stringBuilder.Append("[");
+            IEnumerable enumerable = (IEnumerable)obj;
+            bool isFirst = true;
+            foreach (var item in enumerable)
+            {
+                if (!isFirst)
+                {
+                    stringBuilder.Append(", ");
+                }
+                SerializeValue(item, stringBuilder);
+                isFirst = false;
+            }
+            stringBuilder.Append("]");
+        }
+
+        private static void SerializeObjet(object obj, StringBuilder stringBuilder)
+        {
+            stringBuilder.Append("{");
+            PropertyInfo[] properties = obj.GetType().GetProperties();  
+            for (int i = 0; i < properties.Length; i++) 
+            {
+                stringBuilder.Append($"\"{properties[i].Name}\":");
+                SerializeValue(properties[i].GetValue(obj), stringBuilder);  
+                
+                if (i < properties.Length - 1)
+                {
+                    stringBuilder.Append(",");
+                }
+            }
+            stringBuilder.Append("}");
+        }
+
+        private static bool PrimitiveData(Type type)
+        {
+            return type.IsPrimitive || type == typeof(int)
+                || type == typeof(double) || type == typeof(decimal)
+                || type == typeof(char) || type == typeof(string) || type == typeof(DateTime);
+        }
+
+        private static string InputString(string input)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (char c in input)
+            {
+                //if (c == '"' || c == '\\')
+                //{
+                //    stringBuilder.Append('\\');
+                //}
+                stringBuilder.Append(c);
+            }
+            return stringBuilder.ToString();
+        }
     }
 }
--- assignmentOne/JsonFormattingAssignment/Program.cs
+++ assignmentOne/JsonFormattingAssignment/Program.cs
@@ -10,6 +10,63 @@ namespace JsonFormattingAssignment
     {
         static void Main(string[] args)
         {
+            Course course = new Course();
+            course.Title = "Asp.Net";
+            course.Fees = 30000;
+            course.Teacher = new Instructor()
+            {
+                Name = "JalalUddin",
+                Email = "jalaluddin@gmail.com",
+                PresentAddress = new Address()
+                {
+                    Street = "5/A",
+                    City = "Dhaka",
+                    Country = "Bangladesh"
+                },
+                PermanentAddress = new Address()
+                {
+                    Street = "2/Z",
+                    City = "Rajshahi",
+                    Country = "Bangladesh"
+                },
+                Phones = new List<Phone>()
+                {
+                    new Phone() {Number = "01714288861", Extension = "123", CountryCode = "+880"},
+                    new Phone() {Number = "01782072565", Extension = "456", CountryCode = "+880"}
+                }
+            };
+            course.Topics = new List<Topic>()
+            {
+                new Topic() {Title = "C#", Description = "Intermeidate Course",
+                    Sessions = new List<Session>()
+{                       new Session() {DurationHour = 60, LearningObjective = "OOP"}
+                    }
+                },
+                new Topic() {Title = "Asp.Net", Description = "Profession Course", 
+                    Sessions = new List<Session>()
+{                       new Session() {DurationHour = 120, LearningObjective = "Software Development"}
+                    } 
+                }
+            };
+            course.Tests = new List<AddmissionTest>()
+            {
+                new AddmissionTest()
+                {
+                    StartDate = new DateTime(2024,05,08),
+                    EndDate = new DateTime(2024,05,20),
+                    TestFees = 1000
+                },
+                new AddmissionTest()
+                {
+                    StartDate = new DateTime(2024,06,10),
+                    EndDate = new DateTime(2024,06,20),
+                    TestFees = 2000
+                }
+            };
+            string JsonString = JsonFormattingAssignment.JsonFormatter.Serialize(course);
+            //Console.WriteLine(JsonString);
+
+            Console.WriteLine("Hello World!");
 
         }
     }
