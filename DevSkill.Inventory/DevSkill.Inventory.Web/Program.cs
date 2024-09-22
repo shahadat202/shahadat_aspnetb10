db80778e03cf37e01e2c09f9301f0fc2510d2b54
MD Shahadat Hossain <sharman.bd0@gmail.com>
2024-09-22 21:20:08 +0530

    completed docker setup

--- DevSkill.Inventory/DevSkill.Inventory.Web/Program.cs
+++ DevSkill.Inventory/DevSkill.Inventory.Web/Program.cs
@@ -57,7 +57,8 @@ try
     });
     #endregion
 
-    //builder.WebHost.UseUrls("https://*:80");
+    builder.WebHost.UseUrls("http://*:80");
+
 
     builder.Services.AddIdentity();
     builder.Services.AddControllersWithViews();
