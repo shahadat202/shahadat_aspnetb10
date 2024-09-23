ebc7b862f48d2a5024d95881065b9eae9a3d209c
MD Shahadat Hossain <sharman.bd0@gmail.com>
2024-09-24 00:38:26 +0530

    Added a grid icon in Items page.

--- DevSkill.Inventory/DevSkill.Inventory.Web/wwwroot/js/site.js
+++ DevSkill.Inventory/DevSkill.Inventory.Web/wwwroot/js/site.js
@@ -1,3 +1,21 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+
+//-- Item page content ---  
+// Toggle modal on grid icon click
+document.getElementById('gridIcon').addEventListener('click', function () {
+    const modal = document.getElementById('layoutModal');
+    if (modal.style.display === 'block') {
+        modal.style.display = 'none';
+    } else {
+        modal.style.display = 'block';
+    }
+});
+// Close modal when clicking outside of it
+window.addEventListener('click', function (e) {
+    const modal = document.getElementById('layoutModal');
+    if (!modal.contains(e.target) && e.target.id !== 'gridIcon') {
+        modal.style.display = 'none';
+    }
+});
